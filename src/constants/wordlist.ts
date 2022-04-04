c9e82691ecd32d8058acbe9eead3313c21fdd58e
GrahamSH <grahamshllk@gmail.com>
2022-04-04 13:59:06 +0000

    Better code

--- src/constants/wordlist.ts
+++ src/constants/wordlist.ts
@@ -1,4 +1,5 @@
+let weekend = ['', '']
 export const WORDS = {
-  seventh: ['meses', 'lejos', 'quien',"","", 'trece', 'llegar'],
+  seventh: ['meses', 'lejos', 'quien', ...weekend, 'trece', 'llegar'],
   eighth: ['meses'],
 }
